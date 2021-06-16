728bdf2fd5b6fae0ddfd079c9cd79826a95830f8
cmejia <comejia@teco.com.ar>
2021-06-16 15:44:47 -0300

    Implementacion punto 1

--- haskellate/src/Library.hs
+++ haskellate/src/Library.hs
@@ -3,3 +3,41 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+
+-- Punto 1
+
+type Ingrediente = String
+type Gramos = Number
+type Porcentaje = Number
+
+data Chocolate = Chocolate {
+    nombre :: String,
+    ingredientes :: [Ingrediente],
+    peso :: Gramos,
+    porcentajeDeCacao :: Porcentaje,
+    porcentajeDeAzucar :: Porcentaje
+}
+
+type Precio = Number
+
+esChocolateAmargo :: Chocolate -> Bool
+esChocolateAmargo = (> 60) . porcentajeDeCacao
+
+esAptoDiabeticos :: Chocolate -> Bool
+esAptoDiabeticos = (== 0) . porcentajeDeAzucar
+
+precioPremium :: Chocolate -> Precio
+precioPremium chocolate
+    | esAptoDiabeticos chocolate                            = 8
+    | otherwise                                             = 5
+
+cantidadDeIngredientes :: Chocolate -> Number
+cantidadDeIngredientes = length . ingredientes
+
+precioDeChocolate :: Chocolate -> Precio
+precioDeChocolate chocolate
+    | esChocolateAmargo chocolate                           = peso chocolate * precioPremium chocolate
+    | cantidadDeIngredientes chocolate > 4                  = 8 * cantidadDeIngredientes chocolate
+    | otherwise                                             = 1.5 * peso chocolate
+
--- haskellate/src/Spec.hs
+++ haskellate/src/Spec.hs
@@ -3,9 +3,23 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+-- Test Data
+chocolateAmargo = Chocolate "Muy amargo" [] 10 70 20
+chocolateAmargoYAptoDiabetico = Chocolate "Muy amargo" [] 10 70 0
+chocolateConMuchosIngredientes = Chocolate "El mejor" ["cacao", "limon", "naranja", "pera", "mani"] 10 30 20
+chocolateConPocosIngredientes = Chocolate "Simple" ["cacao", "limon"] 10 30 20
+
+
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+-- Tests Punto 1
+  describe "Test Precio de chocolate" $ do
+    it "Dado un chocolate amargo, entonces su precio es $50" $ do
+      precioDeChocolate chocolateAmargo `shouldBe` 50
+    it "Dado un chocolate amargo y apto para diabeticos, entonces su precio es $80" $ do
+      precioDeChocolate chocolateAmargoYAptoDiabetico `shouldBe` 80
+    it "Dado un chocolate cuya cantidad de ingredientes es mayor al limite, entonces su precio es $40" $ do
+      precioDeChocolate chocolateConMuchosIngredientes `shouldBe` 40
+    it "Dado un chocolate cuya cantidad de ingredientes es menor al limite, entonces su precio es $15" $ do
+      precioDeChocolate chocolateConPocosIngredientes `shouldBe` 15
 
