f4b465353928e89edf9e2762a7c7c5fff30c71f5
cmejia <comejia@teco.com.ar>
2021-06-30 16:56:39 -0300

    Punto 1

--- funcional/parciales/huber2/src/Library.hs
+++ funcional/parciales/huber2/src/Library.hs
@@ -1,5 +1,25 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+
+data Cliente = Cliente {
+    nombreCliente :: String,
+    direccion :: String
+}
+
+type Condicion = Number
+data Chofer = Chofer {
+    nombreChofer :: String,
+    kmAuto :: Number,
+    viajesQueTomo :: [Viaje],
+    condicion :: Condicion
+}
+
+type Dia = Number
+type Mes = Number
+data Viaje = Viaje {
+    fecha :: (Dia, Mes),
+    cliente :: Cliente,
+    costo :: Number
+}
+
