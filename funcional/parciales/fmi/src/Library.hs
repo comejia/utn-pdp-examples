8d8ffada968c16e982f48cd7113e437967e02bfc
cmejia <comejia@teco.com.ar>
2021-06-30 12:41:49 -0300

    Mejoras de resolucion

--- funcional/parciales/fmi/src/Library.hs
+++ funcional/parciales/fmi/src/Library.hs
@@ -6,29 +6,29 @@ import PdePreludat
 type Recurso = String
 
 data Pais = Pais {
-    ipc :: Number,
-    poblacionSectorPublico :: Number,
-    poblacionSectorPrivado :: Number,
+    ingresoPerCapita :: Number,
+    activosPublico :: Number,
+    activosPrivado :: Number,
     recursos :: [Recurso],
     deuda :: Number
 } deriving (Show, Eq)
 
 -- Punto 1b
 namibia :: Pais
-namibia = Pais 4140 400000 650000 ["Mineria", "Ecoturismo"] 50000000
+namibia = Pais 4140 400000 650000 ["Mineria", "Ecoturismo"] 50
 
 
 -- Punto 2
 reducirSectorPublico :: Number -> Pais -> Pais
-reducirSectorPublico puestosAReducir pais = pais { poblacionSectorPublico = poblacionSectorPublico pais - puestosAReducir }
+reducirSectorPublico puestosAReducir pais = pais { activosPublico = activosPublico pais - puestosAReducir }
 
 poblacionActiva :: Pais -> Number
-poblacionActiva pais = poblacionSectorPublico pais + poblacionSectorPrivado pais
+poblacionActiva pais = activosPublico pais + activosPrivado pais
 
-reducirIPC :: Pais -> Pais
-reducirIPC pais
-    | poblacionActiva pais > 100       = pais { ipc = ipc pais * 0.8 }
-    | otherwise                         = pais { ipc = ipc pais * 0.85 }
+reducirIPC :: Number -> Pais -> Pais
+reducirIPC puestos pais
+    | puestos > 100                 = pais { ingresoPerCapita = ingresoPerCapita pais * 0.8 }
+    | otherwise                     = pais { ingresoPerCapita = ingresoPerCapita pais * 0.85 }
 
 modificarDeuda :: Number -> Pais -> Pais
 modificarDeuda dinero pais = pais { deuda = deuda pais + dinero}
@@ -37,28 +37,28 @@ quitarRecurso :: Recurso -> Pais -> Pais
 quitarRecurso recurso pais = pais { recursos = (filter (/= recurso) . recursos) pais }
 
 productoBrutoInterno :: Pais -> Number
-productoBrutoInterno pais = ipc pais * poblacionActiva pais
+productoBrutoInterno pais = ingresoPerCapita pais * poblacionActiva pais
 
 type Estrategia = Pais -> Pais
 
 prestarMillonesDeDolares :: Number -> Estrategia
 prestarMillonesDeDolares prestamo = modificarDeuda (prestamo * 1.5)
 
-redudirPuestosEnSectorPublico :: Number -> Estrategia
-redudirPuestosEnSectorPublico puestosAReducir = reducirIPC . reducirSectorPublico puestosAReducir
+reducirPuestosEnSectorPublico :: Number -> Estrategia
+reducirPuestosEnSectorPublico puestosAReducir = reducirIPC puestosAReducir . reducirSectorPublico puestosAReducir
 
 darAlgunRecursoNatural :: Recurso -> Estrategia
-darAlgunRecursoNatural recurso = quitarRecurso recurso . modificarDeuda (-2000000)
+darAlgunRecursoNatural recurso = quitarRecurso recurso . modificarDeuda (-20)
 
-establecerUnBlindaje :: Estrategia
-establecerUnBlindaje pais = (modificarDeuda (productoBrutoInterno pais / 2) . reducirSectorPublico 500) pais
+establecerBlindaje :: Estrategia
+establecerBlindaje pais = (prestarMillonesDeDolares (productoBrutoInterno pais * 0.5) . reducirPuestosEnSectorPublico 500) pais
 
 
 -- Punto 3a
 type Receta = [Estrategia]
 
 receta :: Receta
-receta = [prestarMillonesDeDolares 200000000, darAlgunRecursoNatural "Mineria"]
+receta = [prestarMillonesDeDolares 200, darAlgunRecursoNatural "Mineria"]
 
 -- Punto 3b
 aplicarReceta :: Pais -> Receta -> Pais
@@ -84,7 +84,7 @@ deudaTotalAFavorDelFMI' = sum . map (deuda)
 listaDeRecetaOrdenada :: Pais -> [Receta] -> Bool
 listaDeRecetaOrdenada _ [unaReceta] = True
 listaDeRecetaOrdenada pais (receta1:receta2:recetas)
-    | (productoBrutoInterno . aplicarReceta pais) receta1 < (productoBrutoInterno . aplicarReceta pais) receta2         = listaDeRecetaOrdenada pais (receta1:recetas)
+    | (productoBrutoInterno . aplicarReceta pais) receta1 <= (productoBrutoInterno . aplicarReceta pais) receta2        = listaDeRecetaOrdenada pais (receta2:recetas)
     | otherwise                                                                                                         = False
 
 
@@ -93,10 +93,11 @@ recursosNaturalesInfinitos :: [Recurso]
 recursosNaturalesInfinitos = "Energia" : recursosNaturalesInfinitos
 
 paisConRecursosInfinitos :: Pais
-paisConRecursosInfinitos = Pais 4140 400000 650000 recursosNaturalesInfinitos 50000000
+paisConRecursosInfinitos = Pais 4140 400000 650000 recursosNaturalesInfinitos 50
 
 -- Punto 6a
--- El algoritmo diverge ya que nunca termina de evaluar la lista de recursos
+-- El algoritmo diverge ya que nunca termina al buscar "Mineria"
 -- Punto 6b
 -- En este caso no hay problemas ya que se evalua la deuda de un pais
+-- Esta relacionado con evaluacion diferida, solo se evalua lo que se necesita
 
