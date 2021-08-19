ec886684936cb200a3f57c064f2fa6421519bcc8
cmejia <comejia@teco.com.ar>
2021-08-19 00:00:47 -0300

    Mejora codigo. Resolucion punto 5 con listas de functores

--- logico/practica_parciales/el_kioskito/solucion.pl
+++ logico/practica_parciales/el_kioskito/solucion.pl
@@ -18,67 +18,56 @@ atiende(leoC, miercoles, horario(14, 18)).
 atiende(martu, miercoles, horario(23, 24)).
 
 % vale atiende los mismos dias y horarios que dodain y juanC.
-atiende(vale, Dias, Horario):-atiende(dodain, Dias, Horario).
-atiende(vale, Dias, Horario):-atiende(juanC, Dias, Horario).
+atiende(vale, Dia, Horario):-atiende(dodain, Dia, Horario).
+atiende(vale, Dia, Horario):-atiende(juanC, Dia, Horario).
 
 % nadie hace el mismo horario que leoC
 % Por el Principio del universo cerrado no se modela, ya que todo
 % lo que no esta en la base de conocimiento se considera falso
 
 % maiu está pensando si hace el horario de 0 a 8 los martes y miércoles
-atiende(maiu, martes, horario(0, 8)).
-atiende(maiu, miercoles, horario(0, 8)).
+% Por el Principio del universo cerrado no se modela, ya que todo
+% lo que no esta en la base de conocimiento se considera falso
 
 
 % Punto 2: quien atiende el kiosko
-atencion(Quien, Dia, Hora):-atiende(Quien, Dia, horario(Inicial, Final)), between(Inicial, Final, Hora).
+quienAtiende(Quien, Dia, Hora):-
+  atiende(Quien, Dia, horario(HorarioInicial, HorarioFinal)), 
+  between(HorarioInicial, HorarioFinal, Hora).
 
 
 % Punto 3: Forever alone
 foreverAlone(Persona, Dia, Horario):-
-  atencion(Persona, Dia, Horario),
-  not((atencion(Socio, Dia, Horario), Persona \= Socio)).
+  quienAtiende(Persona, Dia, Horario),
+  not((quienAtiende(Socio, Dia, Horario), Persona \= Socio)).
 
 
 % Punto 4: Posibilidades de atencion
 posibleAtencion(Dia, KiosquerosPosibles):-
-  findall(Kiosquero, atiende(Kiosquero, Dia, _), Kiosqueros),
-  quioskerosPosibles(Kiosqueros, Dia, KiosquerosPosibles).
+  findall(Kiosquero, distinct(Kiosquero, quienAtiende(Kiosquero, Dia, _)), Kiosqueros),
+  quioskerosPosibles(Kiosqueros, KiosquerosPosibles).
 
-quioskerosPosibles([], _, []).
-quioskerosPosibles([Kiosquero|Kiosqueros], Dia, [Kiosquero|Posibles]):-
-  atiende(Kiosquero, Dia, _),
-  quioskerosPosibles(Kiosqueros, Dia, Posibles).
-quioskerosPosibles([_|Kiosqueros], Dia, Posibles):-
-  quioskerosPosibles(Kiosqueros, Dia, Posibles).
+quioskerosPosibles([], []).
+quioskerosPosibles([Kiosquero|Kiosqueros], [Kiosquero|Posibles]):-
+  quioskerosPosibles(Kiosqueros, Posibles).
+quioskerosPosibles([_|Kiosqueros], Posibles):-
+  quioskerosPosibles(Kiosqueros, Posibles).
 
 % Punto extra
-% Se utiliza Recursividad y pattern matching
+% findall para generar el conjunto de soluciones que satisfacen un predicado
+% Ademas se utiliza mecanismo de backtracking de Prolog que permite encontrar todas las soluciones posibles.
 
 
 % Punto 5: ventas / suertudas
-venta(dodain, lunes, fecha(10, agosto), golosinas(1200)).
-venta(dodain, lunes, fecha(10, agosto), cigarrillos([jockey])).
-venta(dodain, lunes, fecha(10, agosto), golosinas(50)).
-venta(dodain, miercoles, fecha(12, agosto), bebidas(8, alcoholica)).
-venta(dodain, miercoles, fecha(12, agosto), bebidas(1, noAlcoholica)).
-venta(dodain, miercoles, fecha(12, agosto), golosinas(10)).
-venta(martu, miercoles, fecha(12, agosto), golosinas(1000)).
-venta(martu, miercoles, fecha(12, agosto), cigarrillos([chesterfield, colorado, parisiennes])).
-%venta(martu, miercoles, fecha(12, agosto), cigarrillos(colorado)).
-%venta(martu, miercoles, fecha(12, agosto), cigarrillos(parisiennes)).
-venta(lucas, martes, fecha(11, agosto), golosinas(600)).
-venta(lucas, martes, fecha(18, agosto), bebidas(2, noAlcoholica)).
-venta(lucas, martes, fecha(18, agosto), cigarrillos([derby])).
+venta(dodain, lunes, fecha(10, agosto), [golosinas(1200), cigarrillos([jockey]), golosinas(50)]).
+venta(dodain, miercoles, fecha(12, agosto), [bebidas(8, alcoholica), bebidas(1, noAlcoholica), golosinas(10)]).
+venta(martu, miercoles, fecha(12, agosto), [golosinas(1000), cigarrillos([chesterfield, colorado, parisiennes])]).
+venta(lucas, martes, fecha(11, agosto), [golosinas(600)]).
+venta(lucas, martes, fecha(18, agosto), [bebidas(2, noAlcoholica), cigarrillos([derby])]).
 
 vendedorSuertudo(Persona):-
   venta(Persona, _, _, _),
-  %forall((venta(Persona, Dia, _, Producto), primerVenta(Persona, Dia)), esImportante(Producto)).
-  forall(venta(Persona, _, _, Producto), esImportante(Producto)).
-
-primerVenta(Persona, Dia):-
-  venta(Persona, Dia, _, _),
-  not((venta(Persona, OtroDia, _, _), Dia \= OtroDia)).
+  forall(venta(Persona, _, _, [Producto|_]), esImportante(Producto)).
 
 esImportante(golosinas(Precio)):-Precio > 100.
 esImportante(cigarrillos(Marcas)):-length(Marcas, Cantidad), Cantidad > 2.
