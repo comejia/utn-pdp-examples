aeb9eff80fec5c134aea5c558377b97c89792042
cmejia <comejia@teco.com.ar>
2021-08-30 13:27:10 -0300

    Parte b) finalizada

--- logico/practica_parciales/vocaloid/solucion.pl
+++ logico/practica_parciales/vocaloid/solucion.pl
@@ -3,7 +3,9 @@
 % NOMBRE: Cesar Mejia (comejia)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% vocaloid(Cantante, Cancion)
+% Parte a)
+
+% sabeCantar(Cantante, Cancion)
 % Cancion:
 %   cancion(Tema, Duracion)
 sabeCantar(megurineLuka, cancion(nightFever, 4)).
@@ -37,7 +39,70 @@ esAcelerado(Cantante):-
   not((sabeCantar(Cantante, cancion(_, Duracion)), Duracion > 4)).
 
 
+% Parte b)
 
+% Punto 1
+% concierto(Nombre, Pais, CantidadDeFama, TipoDeConcierto)
+% TipoDeConcierto
+%   gigante(CantidadMinimaDeCanciones, DuracionTotalMinima)
+%   mediano(DuracionTotalMinima)
+%   pequenio(DuracionMinimaDeCancion)
+concierto(mikuExpo, eeuu, 2000, gigante(2, 6)).
+concierto(magicalMirai, japon, 3000, gigante(3, 10)).
+concierto(vocalektVisions, eeuu, 1000, mediano(9)).
+concierto(mikuFest, argentina, 100, pequenio(4)).
+
+% Punto 2
+puedeParticipar(Cantante, Concierto):-
+  vocaloid(Cantante),
+  concierto(Concierto, _, _, Requisito),
+  cumpleRequisito(Cantante, Requisito).
+puedeParticipar(hatsuneMiku, Concierto):-
+  concierto(Concierto, _, _, _).
+
+cumpleRequisito(Cantante, gigante(CantidadMinimaDeCanciones, DuracionTotalMinima)):-
+  cantidadDeTemas(Cantante, TotalCanciones), TotalCanciones > CantidadMinimaDeCanciones,
+  duracionTotalDeTemas(Cantante, DuracionTotal), DuracionTotal > DuracionTotalMinima.
+cumpleRequisito(Cantante, mediano(DuracionTotalMinima)):-
+  duracionTotalDeTemas(Cantante, DuracionTotal), DuracionTotal > DuracionTotalMinima.
+cumpleRequisito(Cantante, pequenio(DuracionMinima)):-
+  sabeCantar(Cantante, cancion(_, Duracion)), Duracion > DuracionMinima.
+
+cantidadDeTemas(Cantante, Total):-
+  findall(Cancion, sabeCantar(Cantante, Cancion), ListaCanciones),
+  length(ListaCanciones, Total).
+
+% Punto 3
+vocaloidMasFamoso(Cantante):-
+  vocaloid(Cantante),
+  famaTotalPorParticipar(Cantante, Total1),
+  not((famaTotalPorParticipar(_, Total2), Total1 < Total2)).
+
+famaTotalPorParticipar(Cantante, FamaTotal):-
+  vocaloid(Cantante),
+  findall(Fama, (puedeParticipar(Cantante, Concierto), concierto(Concierto, _, Fama, _)), ListaFama),
+  sum_list(ListaFama, FamaObtenida),
+  cantidadDeTemas(Cantante, TotalCanciones),
+  FamaTotal is FamaObtenida * TotalCanciones.
+
+% Punto 4
+conoceA(megurineLuka, hatsuneMiku).
+conoceA(megurineLuka, gumi).
+conoceA(gumi, seeU).
+conoceA(seeU, kaito).
+
+unicoQueParticipaDeConcierto(Cantante, Concierto):-
+  not(puedeParticipar(Cantante, Concierto)).
+unicoQueParticipaDeConcierto(Cantante, Concierto):-
+  puedeParticipar(Cantante, Concierto),
+  conoceA(Cantante, Conocido),
+  not(puedeParticipar(Conocido, Concierto)),
+  unicoQueParticipaDeConcierto(Conocido, Concierto).
+
+% Punto 5
+% No tendria que hacer ningun cambio, solo agregar una regla mas de "cumpleRequisito"
+% para validar si el cantante puede o no participar del concierto.
+% Esto fue gracias al uso de functores en el hecho "concierto"
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Consultas a resolver
