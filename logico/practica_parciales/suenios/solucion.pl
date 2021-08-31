1dce2aea58cf37e506e584e8e0cdc9f1266a6324
cmejia <comejia@teco.com.ar>
2021-08-31 09:59:41 -0300

    Modelado parcial Suenios

---
+++ logico/practica_parciales/suenios/solucion.pl
@@ -0,0 +1,105 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Parcial: Sueños
+% NOMBRE: Cesar Mejia (comejia)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Punto 1a
+% cree(Persona, Personaje)
+cree(gabriel, campanita).
+cree(gabriel, magoDeOz).
+cree(gabriel, cavenaghi).
+cree(juan, conejoDePascua).
+cree(macarena, reyesMagos).
+cree(macarena, magoCapria).
+cree(macarena, campanita).
+% "Diego no cree en nadie", no se modela por el principio del Universo Cerrado,
+% ya que todo lo que no esta en la base de conocimiento se considera falso.
+
+% quiere(Persona, Suenio)
+quiere(gabriel, ganarLoteria([5, 9])).
+quiere(gabriel, futbolista(arsenal)).
+quiere(juan, cantante(100000)).
+quiere(macarena, cantante(10000)).
+
+% Punto 1b
+% Entraron en juego el Principio del Universo Cerrado.
+% Hecho individuales y compuestos
+
+
+% Punto 2
+ambisioso(Persona):-
+  dificultadTotalDeSuenios(Persona, Total),
+  Total > 20.
+
+dificultadTotalDeSuenios(Persona, Total):-
+  quiere(Persona, _),
+  findall(Dificultad, dificultadDeSuenio(Persona, _, Dificultad), Dificultades),
+  sum_list(Dificultades, Total).
+
+dificultadDeSuenio(Persona, Suenio, Dificultad):-
+  quiere(Persona, Suenio),
+  dificultad(Suenio, Dificultad).
+
+dificultad(cantante(Ventas), 6):-Ventas > 500000.
+dificultad(cantante(Ventas), 4):-Ventas =< 500000.
+dificultad(ganarLoteria(Numeros), Dificultad):-length(Numeros, Cantidad), Dificultad is 10 * Cantidad.
+dificultad(futbolista(Equipo), 3):-equipoChico(Equipo).
+dificultad(futbolista(Equipo), 16):-not(equipoChico(Equipo)).
+
+equipoChico(arsenal).
+equipoChico(aldosivi).
+
+
+% Punto 3
+tieneQuimica(campanita, Persona):-
+  cree(Persona, campanita),
+  dificultadDeSuenio(Persona, _, Dificultad),
+  Dificultad < 5.
+tieneQuimica(Personaje, Persona):-
+  cree(Persona, Personaje), Personaje \= campanita,
+  tieneSueniosPuros(Persona),
+  not(ambisioso(Persona)).
+
+tieneSueniosPuros(Persona):-forall(quiere(Persona, Suenio), esPuro(Suenio)).
+
+esPuro(futbolista(_)).
+esPuro(cantante(Ventas)):-Ventas < 200000.
+
+
+% Punto 4
+amiga(campanita, reyesMagos).
+amiga(campanita, conejoDePascua).
+amiga(conejoDePascua, cavenaghi).
+
+alegra(Personaje, Persona):-
+  quiere(Persona, _),
+  tieneQuimica(Personaje, Persona),
+  not(enfermo(Personaje)).
+alegra(Personaje, Persona):-
+  quiere(Persona, _),
+  tieneQuimica(Personaje, Persona),
+  amistades(Personaje, OtroPersonaje),
+  not(enfermo(OtroPersonaje)).
+
+amistades(Personaje, OtroPersonaje):-
+  amiga(Personaje, OtroPersonaje).
+amistades(Personaje, OtroPersonaje):-
+  amiga(Personaje, UnPersonaje),
+  amistades(UnPersonaje, OtroPersonaje).
+
+enfermo(campanita).
+enfermo(reyesMagos).
+enfermo(conejoDePascua).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Consultas a resolver
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+:- begin_tests(suenios).
+
+  test(fakeTest):-
+    true.
+
+:- end_tests(suenios).
